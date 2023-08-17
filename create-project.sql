0b4c89cb69d20fde05386683a5f463c5a17b7b63
Orlaith McMenamin <orlaith.mcmenamin@kainos.com>
2023-08-17 15:10:51 +0100

    updated SQL file

--- create-project.sql
+++ create-project.sql
@@ -7,3 +7,24 @@ ProjectID int PRIMARY KEY auto_increment,
 Name varchar(60),
 Value DECIMAL (2),
 Completed tinyint);
+
+ALTER TABLE Project 
+ADD TechnologiesID int,
+ADD TechLeadID int,
+ADD ClientID int;
+;
+
+ALTER TABLE Project 
+ADD CONSTRAINT fk_project_technologies_id 
+FOREIGN KEY (TechnologiesID) 
+REFERENCES Technologies(TechnologiesID);
+
+ALTER TABLE Project 
+ADD CONSTRAINT fk_project_techlead_id 
+FOREIGN KEY (TechLeadID) 
+REFERENCES TechLead(TechLeadID);
+
+ALTER TABLE Project 
+ADD CONSTRAINT fk_project_client_id 
+FOREIGN KEY (ClientID) 
+REFERENCES Client(ClientID);
