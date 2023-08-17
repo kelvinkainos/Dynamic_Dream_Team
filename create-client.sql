986e31b6d3c0fde41b10b1ad33303df4d728e4e0
Orlaith McMenamin <orlaith.mcmenamin@kainos.com>
2023-08-17 15:17:41 +0100

    updated create-client SQL file

--- create-client.sql
+++ create-client.sql
@@ -7,3 +7,11 @@ ClientID int PRIMARY KEY auto_increment,
 Name varchar(50),
 Address varchar(200),
 PhoneNumber varchar(25));
+
+ALTER TABLE `Client` 
+ADD AsignedEmployeeID int;
+
+ALTER TABLE `Client` 
+ADD CONSTRAINT fk_client_asignedemployee_id 
+FOREIGN KEY (AsignedEmployeeID) 
+REFERENCES AsignedEmployees(AsignedEmployeeID);
